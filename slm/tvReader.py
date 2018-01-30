72e287ebdf6b168d449e377681080c07cf184016
Steve Winslow <swinslow@gmail.com>
2018-01-30 18:06:05 -0500

    Continued unit tests and implementation for tag-value reader

--- slm/tvReader.py
+++ slm/tvReader.py
@@ -20,32 +20,110 @@
 
 class TVReader:
   # Possible reader state values
-  # ready to parse new tag/value pair
+  # ready to read new tag/value pair
   STATE_READY = 1
-  # in the middle of parsing a multi-line <text> value
+  # in the middle of reading a multi-line <text> value
   STATE_MIDTEXT = 2
   # encountered an error from which we can't recover
   STATE_ERROR = 99
 
   def __init__(self):
     super(TVReader, self).__init__()
-    self.reset()
+    self._reset()
 
-  def reset(self):
+  ##### Main tag-value reading functions
+  ##### External usage shouldn't require calling anything except these
+
+  def readNextLine(self, line):
+    self.currentLine += 1
+
+    if self.state == self.STATE_READY:
+      self._readNextLineFromReady(line)
+    elif self.state == self.STATE_MIDTEXT:
+      self._readNextLineFromMidtext(line)
+    elif self.state == self.STATE_ERROR:
+      return
+    else:
+      # invalid state, set to error state
+      self.state = self.STATE_ERROR
+
+  def isError(self):
+    return self.state == self.STATE_ERROR
+
+  ##### Tag-value reading main helper functions
+
+  def _readNextLineFromReady(self, line):
+    # strip whitespace from beginning of line
+    line = line.lstrip()
+
+    # ignore comment lines
+    if line.startswith("#"):
+      return
+
+    # scan for a colon to split tag and value
+    colonLoc = line.find(':')
+
+    # if no colon found, this is an error
+    if colonLoc == -1:
+      self.state = self.STATE_ERROR
+      return
+
+    # preceding string becomes tag
+    self.currentTag = line[0:colonLoc].strip()
+
+    # subsequent string becomes value, or start of value if multi-line <text>
+    lineRemainder = line[colonLoc+1:]
+    startTextLoc = lineRemainder.find("<text>")
+    if startTextLoc == -1:
+      # no <text>, so just do this value as a single line
+      self.currentValue = lineRemainder.strip()
+      # we'll fall through to add this to the tag-value list below
+    else:
+      # did find <text>, so determine whether to start multi-line reading
+      # exclude the <text> tag itself
+      self.currentValue = lineRemainder[startTextLoc+6:]
+      # is there a closing </text>?
+      endTagLoc = self.currentValue.find("</text>")
+      if endTagLoc == -1:
+        # no closing </text>, so go to multi-line reading
+        self.state = self.STATE_MIDTEXT
+        return
+      else:
+        # found a closing </text>, so just one line
+        # extract the value prior to </text>
+        self.currentValue = self.currentValue[:endTagLoc]
+
+    # if we got here, the value was a single line (maybe with opening and
+    # closing <text>)
+    # add to tag-value list as a tuple
+    t = (self.currentTag, self.currentValue)
+    self.tvList.append(t)
+    # and reset current tag and current value
+    self._resetCurrentTagValue()
+
+  def _readNextLineFromMidtext(self, line):
+    endTagLoc = line.find("</text>")
+    if endTagLoc == -1:
+      # no closing </text>, so continue multiline
+      self.currentValue += line + '\n'
+    else:
+      # found closing </text> so end multiline and record this tag-value
+      self.currentValue += line[0:endTagLoc]
+      t = (self.currentTag, self.currentValue)
+      self.tvList.append(t)
+      # reset current tag and current value, and go back to ready state
+      self._resetCurrentTagValue()
+      self.state = self.STATE_READY
+
+  ##### Other helper functions
+
+  def _reset(self):
     self.state = self.STATE_READY
     self.tvList = []
     self.currentLine = 0
     self.currentTag = ""
     self.currentValue = ""
 
-  def _parseNextLineFromReady(self, line):
-    pass
-
-  def _parseNextLineFromMidtext(self, line):
-    pass
-
-  def parseNextLine(self, line):
-    if self.state == self.STATE_READY:
-      self._parseNextLineFromReady(line)
-    elif self.state == self.STATE_MIDTEXT:
-      self._parseNextLineFromMidtext(line)
+  def _resetCurrentTagValue(self):
+    self.currentTag = ""
+    self.currentValue = ""
--- tests/unit_tvreader.py
+++ tests/unit_tvreader.py
@@ -40,14 +40,164 @@ class TVReaderTestSuite(unittest.TestCase):
     self.assertEqual(self.reader.currentTag, "")
     self.assertEqual(self.reader.currentValue, "")
 
-  @mock.patch('slm.tvReader.TVReader._parseNextLineFromReady')
+  @mock.patch('slm.tvReader.TVReader._readNextLineFromReady')
   def test_will_call_correct_helper_for_ready_state(self, ready_mock):
     self.reader.state = self.reader.STATE_READY
-    self.reader.parseNextLine("test")
+    self.reader.readNextLine("test")
     ready_mock.assert_called_with("test")
 
-  @mock.patch('slm.tvReader.TVReader._parseNextLineFromMidtext')
+  @mock.patch('slm.tvReader.TVReader._readNextLineFromMidtext')
   def test_will_call_correct_helper_for_midtext_state(self, midtext_mock):
     self.reader.state = self.reader.STATE_MIDTEXT
-    self.reader.parseNextLine("test")
+    self.reader.readNextLine("test")
     midtext_mock.assert_called_with("test")
+
+  @mock.patch('slm.tvReader.TVReader._readNextLineFromReady')
+  @mock.patch('slm.tvReader.TVReader._readNextLineFromMidtext')
+  def test_will_not_call_helpers_for_error_state(self, m_mock, r_mock):
+    self.reader.state = self.reader.STATE_ERROR
+    self.reader.readNextLine("test")
+    m_mock.assert_not_called()
+    r_mock.assert_not_called()
+
+  @mock.patch('slm.tvReader.TVReader._readNextLineFromReady')
+  @mock.patch('slm.tvReader.TVReader._readNextLineFromMidtext')
+  def test_will_not_call_helpers_for_read_in_invalid_state(self, m_mock, r_mock):
+    self.reader.state = 17
+    self.reader.readNextLine("test")
+    m_mock.assert_not_called()
+    r_mock.assert_not_called()
+
+  def test_will_set_error_state_for_read_in_invalid_state(self):
+    self.reader.state = 17
+    self.reader.readNextLine("test")
+    self.assertEqual(self.reader.STATE_ERROR, self.reader.state)
+
+  def test_current_line_increases_on_each_read_call(self):
+    self.reader.readNextLine("Tag: value")
+    self.assertEqual(1, self.reader.currentLine)
+    self.reader.currentLine = 23
+    self.reader.readNextLine("Tag: value")
+    self.assertEqual(24, self.reader.currentLine)
+
+  def test_helper_will_reset_current_tag_and_value(self):
+    self.reader.currentTag = "hi"
+    self.reader.currentValue = "there"
+    self.reader._resetCurrentTagValue()
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_ready_can_read_single_tag_value(self):
+    self.reader.readNextLine("Tag:value")
+    self.assertEqual(self.reader.tvList, [("Tag", "value")])
+    self.assertEqual(self.reader.currentLine, 1)
+    # it doesn't adjust currentTag or currentValue b/c we aren't mid-<text>
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_ready_can_strip_whitespace_from_value(self):
+    self.reader.readNextLine("Tag:   value  ")
+    self.assertEqual(self.reader.tvList, [("Tag", "value")])
+
+  def test_ready_cannot_read_line_with_no_colon(self):
+    self.reader.readNextLine("No colon is an error")
+    self.assertEqual(self.reader.tvList, [])
+    self.assertEqual(self.reader.state, self.reader.STATE_ERROR)
+
+  def test_ready_text_tag_switches_to_midtext_state(self):
+    self.reader.readNextLine("Tag: <text>This begins a multiline value")
+    self.assertEqual(self.reader.state, self.reader.STATE_MIDTEXT)
+    self.assertEqual(self.reader.currentLine, 1)
+    self.assertEqual(self.reader.currentTag, "Tag")
+    self.assertEqual(self.reader.currentValue, "This begins a multiline value")
+    # nothing is added to the tag-value list until we close the <text> value
+    self.assertEqual(self.reader.tvList, [])
+
+  def test_ready_text_tag_and_closing_tag_in_one_line_finishes_read(self):
+    self.reader.readNextLine("Tag: <text>Just one line</text>")
+    self.assertEqual(self.reader.state, self.reader.STATE_READY)
+    self.assertEqual(self.reader.tvList, [("Tag", "Just one line")])
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_midtext_continues_if_no_closing_text(self):
+    self.reader.state = self.reader.STATE_MIDTEXT
+    self.reader.currentTag = "Multiline"
+    self.reader.currentValue = "First line\n"
+    self.reader.readNextLine("Second line")
+    # should still be in midtext
+    self.assertEqual(self.reader.state, self.reader.STATE_MIDTEXT)
+    self.assertEqual(self.reader.tvList, [])
+    self.assertEqual(self.reader.currentTag, "Multiline")
+    self.assertEqual(self.reader.currentValue, "First line\nSecond line\n")
+
+  def test_midtext_finishes_if_reaching_closing_text(self):
+    self.reader.state = self.reader.STATE_MIDTEXT
+    self.reader.currentTag = "Multiline"
+    self.reader.currentValue = "First line\n"
+    self.reader.readNextLine("Second line</text>")
+    # should finish and go back to ready, without final line break
+    self.assertEqual(self.reader.state, self.reader.STATE_READY)
+    self.assertEqual(self.reader.tvList, [("Multiline", "First line\nSecond line")])
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_ready_ignores_comment_lines(self):
+    self.reader.readNextLine("# this is a comment")
+    self.assertEqual(self.reader.state, self.reader.STATE_READY)
+    self.assertEqual(self.reader.tvList, [])
+    self.assertEqual(self.reader.currentLine, 1)
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_midtext_includes_comment_lines(self):
+    self.reader.state = self.reader.STATE_MIDTEXT
+    self.reader.currentTag = "Multiline"
+    self.reader.currentValue = "First line\n"
+    self.reader.readNextLine("# This is part of multiline text")
+    # should still be in midtext
+    self.assertEqual(self.reader.state, self.reader.STATE_MIDTEXT)
+    self.assertEqual(self.reader.tvList, [])
+    self.assertEqual(self.reader.currentTag, "Multiline")
+    self.assertEqual(self.reader.currentValue, "First line\n# This is part of multiline text\n")
+
+  def test_ready_ignores_spaces_before_tag(self):
+    self.reader.readNextLine("      Tag:value")
+    self.assertEqual(self.reader.tvList, [("Tag", "value")])
+    self.assertEqual(self.reader.currentLine, 1)
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_ready_ignores_spaces_before_comment_lines(self):
+    self.reader.readNextLine("         # this is a comment")
+    self.assertEqual(self.reader.state, self.reader.STATE_READY)
+    self.assertEqual(self.reader.tvList, [])
+    self.assertEqual(self.reader.currentLine, 1)
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_ready_ignores_spaces_between_tag_and_colon(self):
+    self.reader.readNextLine("Tag     :value")
+    self.assertEqual(self.reader.tvList, [("Tag", "value")])
+    self.assertEqual(self.reader.currentLine, 1)
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_ready_ignores_spaces_between_colon_and_value(self):
+    self.reader.readNextLine("Tag:      value")
+    self.assertEqual(self.reader.tvList, [("Tag", "value")])
+    self.assertEqual(self.reader.currentLine, 1)
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_ready_ignores_spaces_after_end_of_value(self):
+    self.reader.readNextLine("Tag:value     ")
+    self.assertEqual(self.reader.tvList, [("Tag", "value")])
+    self.assertEqual(self.reader.currentLine, 1)
+    self.assertEqual(self.reader.currentTag, "")
+    self.assertEqual(self.reader.currentValue, "")
+
+  def test_can_check_whether_in_error_state(self):
+    self.assertFalse(self.reader.isError())
+    self.reader.state = self.reader.STATE_ERROR
+    self.assertTrue(self.reader.isError())
