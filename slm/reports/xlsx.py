4ae1f00775923435b584c3fcf755ba58d529ec2b
Steve Winslow <swinslow@gmail.com>
2018-02-17 10:52:59 -0500

    Added unit tests and implementation for xlsx reporting and saving

--- slm/reports/xlsx.py
+++ slm/reports/xlsx.py
@@ -18,6 +18,7 @@
 # limitations under the License.
 
 import os
+from collections import OrderedDict
 import openpyxl
 
 from .common import ReportFileError, ReportNotReadyError
@@ -31,12 +32,80 @@ class ExcelReporter:
   ##### Main xlsx reporting functions
   ##### External usage shouldn't require calling anything except these
 
+  def setResults(self, results):
+    self.wb = openpyxl.Workbook()
+    self.results = results
+
+  def generate(self):
+    if type(self.results) != OrderedDict:
+      raise ReportNotReadyError("Cannot call generate() before analysis results are set")
+    self._generateCategorySheets(self.wb, self.results)
+    self._generateFileListings(self.wb, self.results)
+    self.reportGenerated = True
+
   def save(self, path, replace=False):
-    if not self.isReady:
-      raise ReportNotReadyError("Cannot call save() before report is ready")
+    self._saveCheck(path=path, replace=replace)
+    try:
+      self.wb.save(path)
+    except PermissionError:
+      raise ReportFileError(f"Permission denied to save to {path}")
+
+  ##### Helper functions for xlsx reporting
+
+  def _generateCategorySheets(self, wb, results):
+    # first sheet already exists; rename it when we come to one with files
+    first_sheet = True
+    for cat in results.values():
+      # skip category if it has no files
+      if not cat.hasFiles:
+        continue
+
+      # change sheet name or add new sheet, as appropriate
+      if first_sheet:
+        ws = wb.active
+        ws.title = cat.name
+        first_sheet = False
+      else:
+        ws = wb.create_sheet(cat.name)
+
+      # and fill in sheet headers
+      ws['A1'] = "File"
+      ws['B1'] = "License"
+
+  def _generateFileListings(self, wb, results):
+    for cat in results.values():
+      if not cat.hasFiles:
+        continue
+      try:
+        ws = wb[cat.name]
+      except KeyError:
+        raise ReportNotReadyError(f"Sheet not found for category {cat.name}; has _generateCategorySheets() been called?")
+      row = 2
+      for lic in cat.licensesSorted.values():
+        if not lic.hasFiles:
+          continue
+        for file in lic.filesSorted.values():
+          ws[f'A{row}'] = file.path
+          ws[f'B{row}'] = lic.name
+          row += 1
+
+  def _saveCheck(self, path, replace=False):
+    if type(self.results) != OrderedDict:
+      raise ReportNotReadyError("Cannot call save() before analysis results are set")
+    if not self.reportGenerated:
+      raise ReportNotReadyError("Cannot call save() before report is generated")
+
+    # check whether requested file already exists
+    if os.path.exists(path) and not replace:
+      raise ReportFileError(f"File already exists at {path}")
+
+    # check whether we have write permission for this path
+    if not os.access(path=path, mode=os.W_OK):
+      raise ReportFileError(f"Permission denied to save to {path}")
 
   ##### Other helper functions
 
   def _reset(self):
-    self.isReady = False
     self.wb = None
+    self.results = None
+    self.reportGenerated = False
--- tests/unit_reportexcel.py
+++ tests/unit_reportexcel.py
@@ -20,6 +20,7 @@
 import os
 import unittest
 from unittest import mock
+from collections import OrderedDict
 import datetime
 
 from openpyxl import Workbook
@@ -38,101 +39,268 @@ class ReportExcelTestSuite(unittest.TestCase):
     self.db.createDB(":memory:")
     self.db.initializeDBTables()
 
-    # insert sample data
-    self.insertSampleCategoryData()
-    self.insertSampleLicenseData()
-    self.insertSampleSubprojectData()
-    self.insertSampleScanData()
-    self.insertSampleFileData()
-
-    # create reporter
+   # create reporter
     self.reporter = ExcelReporter()
 
   def tearDown(self):
     pass
 
-  def insertSampleCategoryData(self):
-    categories = [
-      Category(_id=1, name="a category", order=3),
-      Category(_id=2, name="cat", order=2),
-      Category(_id=3, name="blah category", order=1),
-    ]
-    self.db.session.bulk_save_objects(categories)
-    self.db.session.commit()
+  ##### Test helpers to mimic results from analysis
 
-  def insertSampleLicenseData(self):
-    licenses = [
-      License(_id=1, name="DoAnything", category_id=1),
-      License(_id=2, name="HarshEULA", category_id=2),
-      License(_id=3, name="293PageEULA", category_id=3),
-      License(_id=4, name="DoAnythingNoncommercial", category_id=1),
-    ]
-    self.db.session.bulk_save_objects(licenses)
-    self.db.session.commit()
+  def _buildCategories(self, results):
+    self.cat2 = Category()
+    self.cat2._id = 2
+    self.cat2.name = "catID2"
+    self.cat2.order = 1
+    self.cat2.licensesSorted = OrderedDict()
+    self.cat2.hasFiles = True
+    results[2] = self.cat2
 
-  def insertSampleSubprojectData(self):
-    subprojects = [
-      Subproject(_id=1, name="sub1", desc="subproject 1"),
-    ]
-    self.db.session.bulk_save_objects(subprojects)
-    self.db.session.commit()
+    # no files, cat3 should not appear in reports
+    self.cat3 = Category()
+    self.cat3._id = 3
+    self.cat3.name = "catID3"
+    self.cat3.order = 2
+    self.cat3.licensesSorted = OrderedDict()
+    self.cat3.hasFiles = False
+    results[3] = self.cat3
 
-  def insertSampleScanData(self):
-    scans = [
-      Scan(_id=1, subproject_id=1, scan_dt=datetime.date(2017, 1, 10),
-        desc="new scan"),
-    ]
-    self.db.session.bulk_save_objects(scans)
-    self.db.session.commit()
-    self.scan_id = 1
+    self.cat1 = Category()
+    self.cat1._id = 1
+    self.cat1.name = "catID1"
+    self.cat1.order = 3
+    self.cat1.licensesSorted = OrderedDict()
+    self.cat1.hasFiles = True
+    results[1] = self.cat1
 
-  def insertSampleFileData(self):
-    files = [
-      File(_id=1, scan_id=1, license_id=1, path="/tmp/f1", sha1=None, md5="abcdef", sha256=None),
-      File(_id=2, scan_id=1, license_id=4, path="/tmp/f2", sha1=None, md5=None, sha256="abcdef"),
-      File(_id=3, scan_id=1, license_id=2, path="/tmp/f3", sha1="abcdef", md5=None, sha256=None),
-      File(_id=4, scan_id=1, license_id=2, path="/tmp/f4", sha1=None, md5=None, sha256=None),
-    ]
-    self.db.session.bulk_save_objects(files)
-    self.db.session.commit()
+  def _buildLicenses(self, results):
+    # should sort alphabetically before lic1cat2 in reports
+    self.lic2cat2 = License()
+    self.lic2cat2._id = 2
+    self.lic2cat2.name = "another lic2cat2"
+    self.lic2cat2.category_id = 2
+    self.lic2cat2.filesSorted = OrderedDict()
+    self.lic2cat2.hasFiles = True
+    self.cat2.licensesSorted[2] = self.lic2cat2
+
+    self.lic1cat2 = License()
+    self.lic1cat2._id = 1
+    self.lic1cat2.name = "lic1cat2"
+    self.lic1cat2.category_id = 2
+    self.lic1cat2.filesSorted = OrderedDict()
+    self.lic1cat2.hasFiles = True
+    self.cat2.licensesSorted[1] = self.lic1cat2
+
+    # no files, lic3cat2 should not appear in reports
+    self.lic3cat2 = License()
+    self.lic3cat2._id = 3
+    self.lic3cat2.name = "nope lic3cat2"
+    self.lic3cat2.category_id = 2
+    self.lic3cat2.filesSorted = OrderedDict()
+    self.lic3cat2.hasFiles = False
+    self.cat2.licensesSorted[3] = self.lic3cat2
+
+    # no files, lic4cat3 should not appear in reports
+    self.lic4cat3 = License()
+    self.lic4cat3._id = 4
+    self.lic4cat3.name = "nope lic4cat3"
+    self.lic4cat3.category_id = 3
+    self.lic4cat3.filesSorted = OrderedDict()
+    self.lic4cat3.hasFiles = False
+    self.cat3.licensesSorted[4] = self.lic4cat3
+
+    self.lic5cat1 = License()
+    self.lic5cat1._id = 5
+    self.lic5cat1.name = "a license"
+    self.lic5cat1.category_id = 1
+    self.lic5cat1.filesSorted = OrderedDict()
+    self.lic5cat1.hasFiles = True
+    self.cat1.licensesSorted[5] = self.lic5cat1
+
+  def _buildFiles(self, results):
+    # should sort alphabetically by path before f1
+    self.f2 = File()
+    self.f2._id = 2
+    self.f2.path = "/first/tmp/f2"
+    self.f2.scan_id = 1
+    self.f2.license_id = 2
+    self.f2.findings = {}
+    self.lic2cat2.filesSorted[2] = self.f2
+
+    self.f1 = File()
+    self.f1._id = 1
+    self.f1.path = "/tmp/f1"
+    self.f1.scan_id = 1
+    self.f1.license_id = 2
+    self.f1.findings = {}
+    self.lic2cat2.filesSorted[1] = self.f1
+
+    # should sort after f2 and f1 b/c license ID sorts after theirs
+    self.f3 = File()
+    self.f3._id = 3
+    self.f3.path = "/earliest/tmp/f3"
+    self.f3.scan_id = 1
+    self.f3.license_id = 1
+    self.f3.findings = {}
+    self.lic1cat2.filesSorted[3] = self.f3
+
+    self.f4 = File()
+    self.f4._id = 4
+    self.f4.path = "/tmp/f4"
+    self.f4.scan_id = 1
+    self.f4.license_id = 1
+    self.f4.findings = {}
+    self.lic1cat2.filesSorted[4] = self.f4
+
+    self.f5 = File()
+    self.f5._id = 5
+    self.f5.path = "/tmp/f5"
+    self.f5.scan_id = 1
+    self.f5.license_id = 5
+    self.f5.findings = {}
+    self.lic5cat1.filesSorted[5] = self.f5
+
+  def _getAnalysisResults(self):
+    results = OrderedDict()
+    self._buildCategories(results)
+    self._buildLicenses(results)
+    self._buildFiles(results)
+    return results
 
   ##### Test cases below
 
   def test_new_reporter_is_in_reset_state(self):
-    self.assertFalse(self.reporter.isReady)
+    self.assertIsNone(self.reporter.results)
     self.assertIsNone(self.reporter.wb)
+    self.assertFalse(self.reporter.reportGenerated)
+
+  ##### Reporter setup function tests
+
+  def test_can_set_results_and_create_workbook(self):
+    results = self._getAnalysisResults()
+    self.reporter.setResults(results)
+    self.assertEqual(Workbook, type(self.reporter.wb))
+    self.assertEqual(results, self.reporter.results)
+
+  ##### Reporter generate function tests
+
+  def test_generate_report_fails_if_results_not_ready(self):
+    with self.assertRaises(ReportNotReadyError):
+      self.reporter.generate()
+    self.assertFalse(self.reporter.reportGenerated)
+
+    # even with workbook created, should fail if results aren't ready yet
+    self.reporter.wb = Workbook()
+    with self.assertRaises(ReportNotReadyError):
+      self.reporter.generate()
+    self.assertFalse(self.reporter.reportGenerated)
+
+  @mock.patch('slm.reports.xlsx.ExcelReporter._generateFileListings')
+  @mock.patch('slm.reports.xlsx.ExcelReporter._generateCategorySheets')
+  def test_generate_calls_required_helpers(self, cs_mock, file_mock):
+    results = self._getAnalysisResults()
+    self.reporter.setResults(results)
+    self.reporter.generate()
+    cs_mock.assert_called_with(self.reporter.wb, self.reporter.results)
+    file_mock.assert_called_with(self.reporter.wb, self.reporter.results)
+
+  def test_generate_sets_report_generated_flag(self):
+    results = self._getAnalysisResults()
+    self.reporter.setResults(results)
+    self.reporter.generate()
+    self.assertTrue(self.reporter.reportGenerated)
+
+  def test_can_generate_report_category_pages(self):
+    wb = Workbook()
+    results = self._getAnalysisResults()
+    self.reporter._generateCategorySheets(wb, results)
+
+    # workbook now has the expected category sheets
+    # catID3 does not appear because it has no files
+    self.assertEqual(["catID2", "catID1"], wb.sheetnames)
+    # proper headers are set for each category page
+    for sheet in wb:
+      self.assertEqual("File", sheet['A1'].value)
+      self.assertEqual("License", sheet['B1'].value)
+
+  def test_cannot_generate_files_before_category_sheets_are_generated(self):
+    wb = Workbook()
+    results = self._getAnalysisResults()
+    with self.assertRaises(ReportNotReadyError):
+      self.reporter._generateFileListings(wb, results)
+
+  def test_can_generate_file_listings(self):
+    wb = Workbook()
+    results = self._getAnalysisResults()
+    self.reporter._generateCategorySheets(wb, results)
+    self.reporter._generateFileListings(wb, results)
+
+    # correct files and licenses are in the correct cells
+    ws1 = wb['catID2']
+    self.assertEqual("/first/tmp/f2", ws1['A2'].value)
+    self.assertEqual("another lic2cat2", ws1['B2'].value)
+    self.assertEqual("/tmp/f1", ws1['A3'].value)
+    self.assertEqual("another lic2cat2", ws1['B3'].value)
+    self.assertEqual("/earliest/tmp/f3", ws1['A4'].value)
+    self.assertEqual("lic1cat2", ws1['B4'].value)
+    self.assertEqual("/tmp/f4", ws1['A5'].value)
+    self.assertEqual("lic1cat2", ws1['B5'].value)
+
+    ws2 = wb['catID1']
+    self.assertEqual("/tmp/f5", ws2['A2'].value)
+    self.assertEqual("a license", ws2['B2'].value)
+
+  ##### Reporter save function tests
 
   @mock.patch('slm.reports.xlsx.os.path.exists', return_value=True)
-  def test_save_raises_exception_if_file_already_exists(self, os_exists):
-    # fake that the report is ready
-    self.reporter.wb = Workbook()
-    self.reporter.isReady = True
+  def test_save_check_raises_exception_if_file_already_exists(self, os_exists):
+    results = self._getAnalysisResults()
+    self.reporter.setResults(results)
+    self.reporter.generate()
 
     with self.assertRaises(ReportFileError):
-      self.reporter.save(path="/tmp/fake/existing.xlsx")
+      self.reporter._saveCheck(path="/tmp/fake/existing.xlsx")
 
   @mock.patch('slm.reports.xlsx.os.path.exists', return_value=True)
-  def test_save_doesnt_raise_exception_if_replace_flag_set(self, os_exists):
-    # fake that the report is ready
-    self.reporter.wb = Workbook()
-    self.reporter.isReady = True
+  @mock.patch('slm.reports.xlsx.os.access', return_value=True)
+  def test_save_check_doesnt_raise_exception_if_replace_flag_set(self, os_access, os_exists):
+    results = self._getAnalysisResults()
+    self.reporter.setResults(results)
+    self.reporter.generate()
 
     try:
-      self.reporter.save(path="/tmp/fake/existing.xlsx", replace=True)
+      self.reporter._saveCheck(path="/tmp/fake/existing.xlsx", replace=True)
     except ReportFileError:
       self.fail("ReportFileError raised even though replace flag is True")
 
-  @mock.patch('slm.reports.xlsx.openpyxl.writer.excel.save_workbook')
-  def test_save_fails_if_no_write_permission_for_path(self, save_mock):
-    # fake that the report is ready
-    self.reporter.wb = Workbook()
-    self.reporter.isReady = True
+  @mock.patch('slm.reports.xlsx.os.access', return_value=False)
+  def test_save_check_fails_if_no_write_permission_for_path(self, os_access):
+    results = self._getAnalysisResults()
+    self.reporter.setResults(results)
+    self.reporter.generate()
 
-    save_mock.side_effect = PermissionError
     with self.assertRaises(ReportFileError):
-      self.reporter.save(path="/tmp/readonly/report.xlsx")
+      self.reporter._saveCheck(path="/tmp/readonly/report.xlsx")
 
-  def test_save_fails_if_report_not_ready_yet(self):
-    newReporter = ExcelReporter()
+  def test_save_check_fails_if_report_not_ready_yet(self):
     with self.assertRaises(ReportNotReadyError):
-      newReporter.save(path="whatever")
+      self.reporter._saveCheck(path="whatever")
+
+    # and even with results added, should fail if report not generated yet
+    results = self._getAnalysisResults()
+    self.reporter.setResults(results)
+    with self.assertRaises(ReportNotReadyError):
+      self.reporter._saveCheck(path="whatever")
+
+  @mock.patch('slm.reports.xlsx.openpyxl.workbook.Workbook.save')
+  @mock.patch('slm.reports.xlsx.ExcelReporter._saveCheck')
+  def test_save_calls_save_checker(self, check_mock, save_mock):
+    results = self._getAnalysisResults()
+    self.reporter.setResults(results)
+    self.reporter.generate()
+
+    path = "something"
+    replace = True
+    self.reporter.save(path=path, replace=replace)
+    check_mock.assert_called_with(path=path, replace=replace)
+    save_mock.assert_called_with(path)
