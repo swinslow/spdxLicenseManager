44291d3de384f0a1023f20e1b3788b1fc0b9f9d4
Steve Winslow <swinslow@gmail.com>
2018-03-27 15:54:59 -0400

    Finished unit tests and implementation for SPDX retrieval

--- slm/retriever.py
+++ slm/retriever.py
@@ -20,9 +20,19 @@
 
 from datetime import datetime
 import os
+import shutil
 
 class RetrieverConfigError(Exception):
   """Exception raised for errors in SPDX retriever configuration.
+
+  Attributes:
+    message -- explanation of the error
+  """
+  def __init__(self, message):
+    self.message = message
+
+class RetrieverNotReadyError(Exception):
+  """Exception raised for errors in SPDX retriever order of operations.
 
   Attributes:
     message -- explanation of the error
@@ -36,14 +46,123 @@ class Retriever:
     super(Retriever, self).__init__()
     self._reset()
 
+  ##### Retriever main functions
+
+  def prepareFiles(self):
+    if self.filesPrepared == True:
+      raise RetrieverNotReadyError("Cannot call prepareFiles again on this Retriever")
+    if self.subprojects == {}:
+      raise RetrieverNotReadyError("No subprojects added before call to prepareFiles")
+    if self.search_dir == "":
+      raise RetrieverNotReadyError("Search directory not set before call to prepareFiles")
+    if self.datestr == "":
+      raise RetrieverNotReadyError("Date string not set before call to prepareFiles")
+
+    files = self._getFiles(self.search_dir)
+    for file in files:
+      filepath = os.path.join(self.search_dir, file)
+      for spdx_search in self.subprojects.keys():
+        if self._testFilename(filepath, spdx_search, self.datestr):
+          t = self.subprojects[spdx_search]
+          t[2].append(filepath)
+    self.filesPrepared = True
+
+  def createResults(self):
+    if self.results != {}:
+      raise RetrieverNotReadyError("Cannot call createResults again on this Retriever")
+    if self.project_dir == "":
+      raise RetrieverNotReadyError("Project directory not set before call to createResults")
+
+    self.results["success"] = []
+    self.results["error"] = []
+    for spdx_search, (name, _id, matches) in self.subprojects.items():
+      if len(matches) == 1:
+        dstFilename = self._makeDstFilename(
+          srcPath = matches[0],
+          spdx_search = spdx_search,
+          datestr = self.datestr,
+          subproject_name = name
+        )
+        dstPath = os.path.join(self.project_dir, name, "spdx", dstFilename)
+        self.results["success"].append((_id, name, matches[0], dstPath))
+      elif len(matches) == 0:
+        self.results["error"].append((_id, name, f'No matches found for project {name} (with string "{spdx_search}")'))
+      else:
+        filelistStr = ""
+        for filepath in sorted(matches):
+          filelistStr = filelistStr + "\n  " + filepath
+        self.results["error"].append((_id, name, f'Multiple valid matches found for project {name} (with string "{spdx_search}"):{filelistStr}'))
+
+  def moveFiles(self):
+    if self.results == {}:
+      raise RetrieverNotReadyError("Cannot call moveFiles on this Retriever before successfully calling createResults")
+    success = self.results.get("success", [])
+    newSuccess = []
+    for _id, project, srcPath, dstPath in success:
+      if os.path.isfile(dstPath):
+        t = (_id, project, f'Cannot move to project {project} (file already present at {dstPath})')
+        self.results["error"].append(t)
+      else:
+        shutil.move(srcPath, dstPath)
+        t = (_id, project, srcPath, dstPath)
+        newSuccess.append(t)
+    self.results["success"] = newSuccess
+
+  ##### Retriever helper functions
+
+  def _testFilename(self, filename, spdx_search, datestr):
+    # check that the filename extension is .spdx
+    ext = os.path.splitext(filename)[1]
+    if ext != ".spdx":
+      return False
+    # check that the subproject's search string is present in the filename
+    spdxSearchLoc = filename.find(spdx_search)
+    if spdxSearchLoc == -1:
+      return False
+    # check that the date string is also present
+    dateLoc = filename.find(datestr)
+    if dateLoc == -1:
+      return False
+    # check that the FULL date string (including day) is present and valid
+    dateToCheck = filename[dateLoc:dateLoc+10]
+    try:
+      dt = datetime.strptime(dateToCheck, "%Y-%m-%d")
+    except ValueError as e:
+      return False
+    return True
+
+  def _getFiles(self, search_dir):
+    files = []
+    for pathname in os.listdir(search_dir):
+      if os.path.isfile(pathname):
+        files.append(pathname)
+    return files
+
+  def _makeDstFilename(self, srcPath, spdx_search, datestr, subproject_name):
+    # find day in date
+    # first, check that the date string is present
+    dateLoc = srcPath.find(datestr)
+    if dateLoc == -1:
+      raise RetrieverNotReadyError(f"Date string {datestr} not found in source path {srcPath}")
+    # check that the FULL date string (including day) is present and valid
+    dateToCheck = srcPath[dateLoc:dateLoc+10]
+    try:
+      dt = datetime.strptime(dateToCheck, "%Y-%m-%d")
+    except ValueError as e:
+      raise RetrieverNotReadyError(f"Invalid date string {datestr} found in source path {srcPath}")
+
+    daystr = dateToCheck[8:10]
+    filename = f"{subproject_name}-{datestr}-{daystr}.spdx"
+    return filename
+
   ##### Retriever configuration functions
 
-  def addSubproject(self, spdx_search, _id):
+  def addSubproject(self, name, spdx_search, _id):
     if (spdx_search is None or type(spdx_search) != str or spdx_search == ""):
       raise RetrieverConfigError("spdx_search must be a non-empty string")
     if (_id is None or type(_id) != int or _id < 1):
       raise RetrieverConfigError("_id must be a positive integer")
-    self.subprojects[spdx_search] = (_id, [])
+    self.subprojects[spdx_search] = (name, _id, [])
 
   def setDatestr(self, datestr):
     if datestr == "":
@@ -72,4 +191,5 @@ class Retriever:
     self.datestr = ""
     self.search_dir = ""
     self.project_dir = ""
+    self.filesPrepared = False
     self.results = {}
--- tests/unit_retriever.py
+++ tests/unit_retriever.py
@@ -21,8 +21,33 @@ import os
 import unittest
 from unittest import mock
 
-from slm.retriever import Retriever, RetrieverConfigError
+from slm.retriever import (Retriever, RetrieverConfigError,
+  RetrieverNotReadyError)
 from slm.datatypes import Subproject
+
+##### helper for testing _getFiles and prepareFiles
+TFILES = ['fileone.spdx', 'filetwo', 'hello.sC.blah2018-03-17.blah.spdx']
+TDIRS  = ['dirone', 'dirtwo', 'dirthree']
+TALL   = TFILES + TDIRS
+
+def side_effect_isfile(path):
+  return path in TFILES
+
+##### helper for testing createResults
+MOVEFILES = [
+  'junk.hello.junk.2018-03-12.spdx',
+  'junk.hello.junk.2018-03.spdx',
+  'junk.goodbye.junk.2018-03-20.spdx',
+  'junk.goodbye.junk.2018-03-17.spdx',
+  'junk.bonjour.junk.2018-03-01.spdx',
+]
+MOVE_SUBPROJECTS = [
+  ('hello', 1, 'hello'),
+  ('goodbye', 2, 'goodbye'),
+  ('bonjourProject', 3, 'bonjour'),
+  ('holaPrj', 4, 'hola'),
+]
+
 
 class RetrieverTestSuite(unittest.TestCase):
   """spdxLicenseManager SPDX retrieval unit test suite."""
@@ -41,6 +66,7 @@ class RetrieverTestSuite(unittest.TestCase):
     self.assertEqual(self.retriever.datestr, "")
     self.assertEqual(self.retriever.search_dir, "")
     self.assertEqual(self.retriever.project_dir, "")
+    self.assertEqual(self.retriever.filesPrepared, False)
     self.assertEqual(self.retriever.results, {})
 
   ##### Getter and setter tests
@@ -70,35 +96,35 @@ class RetrieverTestSuite(unittest.TestCase):
       self.retriever.setDatestr("2018-30")
     self.assertEqual("", self.retriever.datestr)
 
-  def test_can_add_subproject_ids_and_spdx_search_strings(self):
-    self.retriever.addSubproject(spdx_search="subA", _id=1)
-    self.retriever.addSubproject(spdx_search="sC", _id=3)
-    self.retriever.addSubproject(spdx_search="subX", _id=2)
-    _id, files = self.retriever.subprojects.get("sC")
+  def test_can_add_subproject_data(self):
+    self.retriever.addSubproject(name="subA", spdx_search="subA", _id=1)
+    self.retriever.addSubproject(name="subC", spdx_search="sC", _id=3)
+    self.retriever.addSubproject(name="subX", spdx_search="subX", _id=2)
+    name, _id, files = self.retriever.subprojects.get("sC")
     self.assertEqual(3, _id)
     self.assertEqual([], files)
 
   def test_cannot_add_subproject_ids_or_spdx_search_with_invalid_types(self):
     with self.assertRaises(RetrieverConfigError):
-      self.retriever.addSubproject(spdx_search="", _id=1)
+      self.retriever.addSubproject(name='test', spdx_search="", _id=1)
     self.assertEqual({}, self.retriever.subprojects)
     with self.assertRaises(RetrieverConfigError):
-      self.retriever.addSubproject(spdx_search=None, _id=1)
+      self.retriever.addSubproject(name='test', spdx_search=None, _id=1)
     self.assertEqual({}, self.retriever.subprojects)
     with self.assertRaises(RetrieverConfigError):
-      self.retriever.addSubproject(spdx_search=1, _id=1)
+      self.retriever.addSubproject(name='test', spdx_search=1, _id=1)
     self.assertEqual({}, self.retriever.subprojects)
     with self.assertRaises(RetrieverConfigError):
-      self.retriever.addSubproject(spdx_search="hi", _id=0)
+      self.retriever.addSubproject(name='test', spdx_search="hi", _id=0)
     self.assertEqual({}, self.retriever.subprojects)
     with self.assertRaises(RetrieverConfigError):
-      self.retriever.addSubproject(spdx_search="hi", _id=-1)
+      self.retriever.addSubproject(name='test', spdx_search="hi", _id=-1)
     self.assertEqual({}, self.retriever.subprojects)
     with self.assertRaises(RetrieverConfigError):
-      self.retriever.addSubproject(spdx_search="hi", _id=None)
+      self.retriever.addSubproject(name='test', spdx_search="hi", _id=None)
     self.assertEqual({}, self.retriever.subprojects)
     with self.assertRaises(RetrieverConfigError):
-      self.retriever.addSubproject(spdx_search="hi", _id="blah")
+      self.retriever.addSubproject(name='test', spdx_search="hi", _id="blah")
     self.assertEqual({}, self.retriever.subprojects)
 
   @mock.patch("slm.retriever.os.path.isdir", return_value=True)
@@ -120,3 +146,195 @@ class RetrieverTestSuite(unittest.TestCase):
     with self.assertRaises(RetrieverConfigError):
       self.retriever.setSearchDir(projectDir)
     self.assertEqual("", self.retriever.project_dir)
+
+  ##### SPDX retrieval filename checker tests
+
+  def test_proper_filenames_match(self):
+    spdx_search = "hello"
+    datestr = "2018-03"
+    self.assertTrue(self.retriever._testFilename(filename="hello-2018-03-08.spdx", spdx_search=spdx_search, datestr=datestr))
+    self.assertTrue(self.retriever._testFilename(filename="hello-other_stuff_2018-03-08-blah-whatever.spdx", spdx_search=spdx_search, datestr=datestr))
+    self.assertTrue(self.retriever._testFilename(filename="datefirst-2018-03-08-blah-hello-ok.spdx", spdx_search=spdx_search, datestr=datestr))
+    self.assertTrue(self.retriever._testFilename(filename="hellohello-2018-03-08.spdx", spdx_search=spdx_search, datestr=datestr))
+    self.assertTrue(self.retriever._testFilename(filename="somethinghelloblah-2018-03-08.spdx", spdx_search=spdx_search, datestr=datestr))
+
+  def test_improper_filenames_do_not_match(self):
+    spdx_search = "hello"
+    datestr = "2018-03"
+    # no subproject search string or date
+    self.assertFalse(self.retriever._testFilename(filename="blah.spdx", spdx_search=spdx_search, datestr=datestr))
+    # no date
+    self.assertFalse(self.retriever._testFilename(filename="hello.spdx", spdx_search=spdx_search, datestr=datestr))
+    # no day
+    self.assertFalse(self.retriever._testFilename(filename="hello-2018-03.spdx", spdx_search=spdx_search, datestr=datestr))
+    # no hyphen between year and month
+    self.assertFalse(self.retriever._testFilename(filename="hello-201803.spdx", spdx_search=spdx_search, datestr=datestr))
+    # no hyphen between month and day
+    self.assertFalse(self.retriever._testFilename(filename="hello-2018-0320.spdx", spdx_search=spdx_search, datestr=datestr))
+    # no subproject search string
+    self.assertFalse(self.retriever._testFilename(filename="2018-03-20.spdx", spdx_search=spdx_search, datestr=datestr))
+    # invalid day
+    self.assertFalse(self.retriever._testFilename(filename="hello-2018-03-40.spdx", spdx_search=spdx_search, datestr=datestr))
+    # no .spdx extension
+    self.assertFalse(self.retriever._testFilename(filename="hello-2018-03-08", spdx_search=spdx_search, datestr=datestr))
+
+  @mock.patch("slm.retriever.os.path.isfile", side_effect=side_effect_isfile)
+  @mock.patch("slm.retriever.os.listdir", return_value=TALL)
+  def test_can_get_files_from_search_directory(self, mock_listdir, mock_isfile):
+    files = self.retriever._getFiles(search_dir="/tmp/fake/whatever")
+    self.assertEqual(TFILES, files)
+
+  @mock.patch("slm.retriever.os.path.isdir", return_value=True)
+  @mock.patch("slm.retriever.os.path.isfile", side_effect=side_effect_isfile)
+  @mock.patch("slm.retriever.os.listdir", return_value=TALL)
+  def test_prepare_fails_if_subprojects_not_set(self, mock_listdir, mock_isfile, mock_isdir):
+    self.retriever.setSearchDir('/tmp/fake/whatever')
+    self.retriever.setDatestr(datestr='2018-03')
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever.prepareFiles()
+
+  @mock.patch("slm.retriever.os.path.isdir", return_value=True)
+  @mock.patch("slm.retriever.os.path.isfile", side_effect=side_effect_isfile)
+  @mock.patch("slm.retriever.os.listdir", return_value=TALL)
+  def test_prepare_fails_if_datestr_not_set(self, mock_listdir, mock_isfile, mock_isdir):
+    self.retriever.setSearchDir('/tmp/fake/whatever')
+    self.retriever.addSubproject(name='subC', spdx_search='sC', _id=1)
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever.prepareFiles()
+
+  @mock.patch("slm.retriever.os.path.isfile", side_effect=side_effect_isfile)
+  @mock.patch("slm.retriever.os.listdir", return_value=TALL)
+  def test_prepare_fails_if_search_dir_not_set(self, mock_listdir, mock_isfile):
+    self.retriever.setDatestr(datestr='2018-03')
+    self.retriever.addSubproject(name='subC', spdx_search='sC', _id=1)
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever.prepareFiles()
+
+  @mock.patch("slm.retriever.os.path.isdir", return_value=True)
+  @mock.patch("slm.retriever.os.path.isfile", side_effect=side_effect_isfile)
+  @mock.patch("slm.retriever.os.listdir", return_value=TALL)
+  def test_cannot_prepare_files_twice(self, mock_listdir, mock_isfile, mock_isdir):
+    self.retriever.setSearchDir('/tmp/fake/whatever')
+    self.retriever.addSubproject(name='subC', spdx_search='sC', _id=1)
+    self.retriever.setDatestr(datestr='2018-03')
+    self.retriever.prepareFiles()
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever.prepareFiles()
+
+  @mock.patch("slm.retriever.os.path.isdir", return_value=True)
+  @mock.patch("slm.retriever.os.path.isfile", side_effect=side_effect_isfile)
+  @mock.patch("slm.retriever.os.listdir", return_value=TALL)
+  @mock.patch("slm.retriever.Retriever._testFilename")
+  def test_prepare_calls_tester_for_each_file_in_search_directory(self, mock_testfilename, mock_listdir, mock_isfile, mock_isdir):
+    self.retriever.setSearchDir('/tmp/fake/whatever')
+    self.retriever.addSubproject(name='subC', spdx_search='sC', _id=1)
+    self.retriever.setDatestr(datestr='2018-03')
+    self.retriever.prepareFiles()
+    expected = [
+      mock.call('/tmp/fake/whatever/fileone.spdx', 'sC', '2018-03'),
+      mock.call('/tmp/fake/whatever/filetwo', 'sC', '2018-03'),
+      mock.call('/tmp/fake/whatever/hello.sC.blah2018-03-17.blah.spdx', 'sC', '2018-03'),
+    ]
+    self.assertEqual(expected, mock_testfilename.call_args_list)
+
+  @mock.patch("slm.retriever.os.path.isdir", return_value=True)
+  @mock.patch("slm.retriever.os.path.isfile", side_effect=side_effect_isfile)
+  @mock.patch("slm.retriever.os.listdir", return_value=TALL)
+  def test_prepare_adds_matches_to_subproject(self, mock_listdir, mock_isfile, mock_isdir):
+    self.retriever.setSearchDir('/tmp/fake/whatever')
+    self.retriever.addSubproject(name='subC', spdx_search='sC', _id=1)
+    self.retriever.setDatestr(datestr='2018-03')
+    self.retriever.prepareFiles()
+    self.assertTrue(self.retriever.filesPrepared)
+    self.assertEqual(
+      {'sC': ('subC', 1, ['/tmp/fake/whatever/hello.sC.blah2018-03-17.blah.spdx'])},
+      self.retriever.subprojects
+    )
+
+  def test_create_results_fails_if_files_not_prepared(self):
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever.createResults()
+
+  def test_create_results_fails_if_results_are_not_empty(self):
+    self.retriever.results = {'junk': 17}
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever.createResults()
+
+  @mock.patch("slm.retriever.os.path.isdir", return_value=True)
+  @mock.patch("slm.retriever.os.path.isfile", return_value=True)
+  @mock.patch("slm.retriever.os.listdir", return_value=MOVEFILES)
+  def test_create_results_fails_if_project_dir_not_set(self, mock_listdir, mock_isfile, mock_isdir):
+    for name, _id, spdx_search in MOVE_SUBPROJECTS:
+      self.retriever.addSubproject(name=name, _id=_id, spdx_search=spdx_search)
+    self.retriever.setSearchDir('/tmp/fake/src')
+    self.retriever.setDatestr(datestr='2018-03')
+    self.retriever.prepareFiles()
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever.createResults()
+
+  @mock.patch("slm.retriever.os.path.isdir", return_value=True)
+  @mock.patch("slm.retriever.os.path.isfile", return_value=True)
+  @mock.patch("slm.retriever.os.listdir", return_value=MOVEFILES)
+  def test_can_create_results_actions(self, mock_listdir, mock_isfile, mock_isdir):
+    for name, _id, spdx_search in MOVE_SUBPROJECTS:
+      self.retriever.addSubproject(name, _id=_id, spdx_search=spdx_search)
+    self.retriever.setSearchDir('/tmp/fake/src')
+    self.retriever.setProjectDir('/tmp/fake/dst')
+    self.retriever.setDatestr(datestr='2018-03')
+    self.retriever.prepareFiles()
+    self.retriever.createResults()
+
+    # check successes
+    success = self.retriever.results.get('success')
+    self.assertEqual((1, 'hello', '/tmp/fake/src/junk.hello.junk.2018-03-12.spdx', '/tmp/fake/dst/hello/spdx/hello-2018-03-12.spdx'), success[0])
+    self.assertEqual((3, 'bonjourProject', '/tmp/fake/src/junk.bonjour.junk.2018-03-01.spdx', '/tmp/fake/dst/bonjourProject/spdx/bonjourProject-2018-03-01.spdx'), success[1])
+
+    # check errors
+    error = self.retriever.results.get('error')
+    self.assertEqual((2, 'goodbye', 'Multiple valid matches found for project goodbye (with string "goodbye"):\n  /tmp/fake/src/junk.goodbye.junk.2018-03-17.spdx\n  /tmp/fake/src/junk.goodbye.junk.2018-03-20.spdx'), error[0])
+    self.assertEqual((4, 'holaPrj', 'No matches found for project holaPrj (with string "hola")'), error[1])
+
+  def test_can_get_dst_filename_from_helper(self):
+    srcPath = "/tmp/whatever/junk.hello.junk.2018-03-12-morejunk.spdx"
+    spdx_search = "hello"
+    datestr = "2018-03"
+    subproject_name = "helloPrj"
+    dstFilename = self.retriever._makeDstFilename(srcPath=srcPath, spdx_search=spdx_search, datestr=datestr, subproject_name=subproject_name)
+    self.assertEqual("helloPrj-2018-03-12.spdx", dstFilename)
+
+  def test_dst_filename_helper_fails_if_invalid_date_in_string(self):
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever._makeDstFilename(srcPath="2018-03.spdx", spdx_search='a',
+        datestr="2018-03", subproject_name='b')
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever._makeDstFilename(srcPath="2018-03-.spdx", spdx_search='a',
+        datestr="2018-03", subproject_name='b')
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever._makeDstFilename(srcPath="2018-03-40.spdx",
+        spdx_search='a', datestr="2018-03", subproject_name='b')
+
+  @mock.patch("slm.retriever.shutil.move")
+  def test_can_move_files(self, mock_move):
+    self.retriever.results["success"] = [
+      (1, 'hello', '/tmp/fake/src/junk.hello.junk.2018-03-12.spdx', '/tmp/fake/dst/hello/spdx/hello-2018-03-12.spdx')
+    ]
+    self.retriever.results["error"] = []
+    self.retriever.moveFiles()
+    mock_move.assert_called_with('/tmp/fake/src/junk.hello.junk.2018-03-12.spdx', '/tmp/fake/dst/hello/spdx/hello-2018-03-12.spdx')
+
+  @mock.patch("slm.retriever.shutil.move")
+  def test_cannot_move_files_before_results_are_set(self, mock_move):
+    with self.assertRaises(RetrieverNotReadyError):
+      self.retriever.moveFiles()
+
+  @mock.patch("slm.retriever.os.path.isfile", return_value=True)
+  @mock.patch("slm.retriever.shutil.move")
+  def test_cannot_move_files_if_already_present_in_dst(self, mock_move, mock_isfile):
+    self.retriever.results["success"] = [
+      (1, 'hello', '/tmp/fake/src/junk.hello.junk.2018-03-12.spdx', '/tmp/fake/dst/hello/spdx/hello-2018-03-12.spdx')
+    ]
+    self.retriever.results["error"] = []
+    self.retriever.moveFiles()
+    # not moved, and now marked as an error
+    mock_move.assert_not_called()
+    self.assertIn((1, 'hello', 'Cannot move to project hello (file already present at /tmp/fake/dst/hello/spdx/hello-2018-03-12.spdx)'), self.retriever.results["error"])
